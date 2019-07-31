90c60a3df303930b60484f6650eb63fb5170c878
mdq <mdq@LAPTOP-QBNKJGV2>
2019-07-31 18:04:53 +0800

    java004

---
+++ src/day004/TestAri.java
@@ -0,0 +1,24 @@
+package day004;
+
+public class TestAri {
+
+	public static void main(String[] args){
+		int ia = 10;
+		int ib = 5;
+		System.out.println("ia = " + ia);
+		System.out.println("ib = " + ib);
+		
+		System.out.println("----------------------");;
+		
+		int res = ia + ib;
+		System.out.println("ia + ib = " + res);
+		
+		System.out.println(ia + ib);
+		System.out.println(ia - ib);
+		System.out.println(ia * ib);
+		System.out.println(ia % ib);
+		System.out.println("----------------");
+//		System.out.println((double)5 / 2);
+//		System.out.println(0.0 / 0.0);
+	}
+}
